924d731fda710e92abf912bb627645f9c001576a
Kamil Kaygisiz <96066271+kaygisizkamil@users.noreply.github.com>
2023-01-24 19:58:07 +0300

    Circular queue using array completed

--- QueueImplementation/src/Queue.java
+++ QueueImplementation/src/Queue.java
@@ -1,55 +1,55 @@
 public class Queue {
     //push back ,pop front
     private int size;
-    private int rear;
     private int front;
+    private int rear;
     private int arr[];
-    public Queue(int size){
+
+    public Queue(int size) {
         arr=new int[size];
-        rear=-1;// to indicate queue is empty assign r and f to -1
         front=-1;
+        rear=-1;
         size=0;
     }
-    public void enqueue(int element){
-        //add the back using rear;
+    public  void enqueue(int data){
         if(isFull()){
-            System.out.println("Queue is full");return;
+            System.out.println("Queue is full");
+            return;
         }
-        ++rear;
+        rear++;
         if(rear==arr.length){
             rear=0;
         }
-        arr[rear]=element;
-        ++size;
-        if(front==-1)++front;//if first element has added  to indicate increment front
-
+        arr[rear]=data;
+        size++;
+        if(front==-1){
+            front=0;//after first element has added to list
+        }
     }
     public int dequeue(){
-        if(isEmpty()){
-            System.out.println("Nothing to return queue is empty");
-            return Integer.MIN_VALUE;
-        }
+        if(isFull()||isEmpty())return Integer.MIN_VALUE;//optionall throw an error
         int data=arr[front];
         if(front==arr.length){
-            front=-1;//to indicate queue is empty again
+            front=0;
+        }
+        size--;
+        if(size==0){
+            front=-1;
             rear=-1;
         }
-        else if(front==arr.length-1){
-                front=0;
-        }
-        else  front++;
-        size--;
         return data;
+
     }
     public int peek(){
-        int data;
-        if(isEmpty() || isFull()) throw new RuntimeException("Do a null control");
-        else {data=arr[front];}
-        System.out.println(data);
-        return data;
+            int data;
+            if(isEmpty() || isFull()) throw new RuntimeException("Do a null control");
+            else {data=arr[front];}
+            System.out.println(data);
+            return data;
     }
     public boolean isFull(){
         return size==arr.length;
+
     }
     public boolean isEmpty(){
         return size==0;
